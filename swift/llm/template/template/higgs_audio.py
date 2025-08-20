4789853f6730fcb3c25015e697194ba6c5c8fb62
Vikram Solanki <vs0439715@gmail.com>
2025-08-20 14:21:36 +0400

    new changes

--- swift/llm/template/template/higgs_audio.py
+++ swift/llm/template/template/higgs_audio.py
@@ -7,16 +7,15 @@ logger = get_logger()
 
 # Create template meta for Higgs-Audio ChatML format
 higgs_chatml_template = TemplateMeta(
-    prefix=['<|im_start|>system\n{{SYSTEM}}<|im_end|>\n'],
+    template_type='higgs-chatml',
+    prefix=[],
     prompt=['<|im_start|>user\n{{QUERY}}<|im_end|>\n<|im_start|>assistant\n'],
-    chat_sep=['\n'],
+    chat_sep=['<|im_end|>\n'],
     suffix=['<|im_end|>'],
+    system_prefix=['<|im_start|>system\n{{SYSTEM}}<|im_end|>\n'],
     default_system='You are a helpful assistant capable of generating speech in the voice of the provided reference audio.',
-    system_prefix=['<|im_start|>system\n'],
-    system_suffix=['<|im_end|>\n'],
     auto_add_bos=True,
     stop_words=['<|im_end|>', '<|im_start|>'],
-    infer_media_type='round',
 )
 
 # Register the template
